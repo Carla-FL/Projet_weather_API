6291725c9b68915a7ceb78b3c49e16a18fac67ca
Carla-FL <carla.flpro@gmail.com>
2025-06-23 16:47:13 +0200

    Update input_models.py

--- src/models/input_models.py
+++ src/models/input_models.py
@@ -2,11 +2,25 @@ from pydantic import BaseModel
 
 
 class OpenWeatherGeocodingModel(BaseModel):
+    name : str
     lat: float
     lon: float
+    country : str
+    state : str
+
 
 class OpenWeatherAPIResponseModel(BaseModel):
-    pass
+    temp : float
+    feels_like : float
+    pressure : int
+    humidity : int
+    dew_point : float
+    uvi : float
+    clouds : int
+    visibility : int
+    wind_speed : float
+    wind_deg : int
+# a incrementer si besoin avec d'autres chmaps 
 
 class OpenMeteoAPIResponseModel(BaseModel):
     pass
