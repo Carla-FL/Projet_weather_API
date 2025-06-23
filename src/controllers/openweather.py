789e897f5a2e37cf587021d9d88bbdd3525cfca6
Carla-FL <carla.flpro@gmail.com>
2025-06-23 17:21:40 +0200

    update load meteo from city

--- src/controllers/openweather.py
+++ src/controllers/openweather.py
@@ -1,7 +1,7 @@
 import httpx
 from src.models.input_models import (
     OpenWeatherGeocoding, 
-    OpenWeatherAPIResponse
+    OpenWeatherAPIResponseModel
 )
 
 def get_coord_from_city(c):
@@ -14,5 +14,24 @@ def get_coord_from_city(c):
 
 def get_weather_from_city(c):
     # step 1 : call geocoding
+    fields = get_coord_from_city(c)
+    lon = fields.lon
+    lat = fields.lat
     # step 2 : call openweatherapi with lon, lat
-    return 
+    URL = f"https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&appid={TOKEN}"
+    res = httpx.get(URL).json()
+
+    if res.status_code == 200:
+        return OpenWeatherAPIResponseModel(
+                temp = res.get('temp'),
+                feels_like = res.get('feels_like'),
+                pressure = res.get('pressure'),
+                humidity = res.get('humidity'),
+                dew_point = res.get('dew_point'),
+                uvi = res.get('uvi'),
+                clouds = res.get('clouds'),
+                visibility = res.get('visibility'),
+                wind_speed = res.get('wind_speed'),
+                wind_deg = res.get('wind_deg')
+                )
+    return None
