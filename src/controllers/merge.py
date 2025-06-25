b4d57711523166255959e3259d510eb6b3e6ec75
Féréol Gbenou <fereolgbenou@MacBook-Air-de-Fereol.local>
2025-06-25 13:20:59 +0200

    implemetantion redis

--- src/controllers/merge.py
+++ src/controllers/merge.py
@@ -1,10 +1,34 @@
+import os
+import redis
+import dotenv
+import datetime
 from src.models.response_models import (
     MultiSourceModelRep, 
     SingleSourceModelRep
 )
 
-def get_multisource_meteo(city_name) -> MultiSourceModelRep:
+dotenv.load_dotenv()
 
+redis_client = redis.Redis(
+    host=os.getenv("redis_host"), 
+    port=int(os.getenv("redis_port")), 
+    db=int(os.getenv("redis_db")))
+
+def flatten(d):
+    _d = dict()
+    for k,v in d.items():
+        if isinstance(v, dict):
+            for vk, vv in v.items():
+                _d[f"{k}/{vk}"] = vv
+        else:
+            _d[k] = v
+    return _d
+
+def make_redis_key(city_name):
+    return f"{city_name}" # composer avec la ville, le pays etc..
+
+def call_multisource_meteo(city_name) -> MultiSourceModelRep:
+    _ = city_name
     res_openmeteo: SingleSourceModelRep = SingleSourceModelRep() # openmeteo.get_meteo()
     res_openmeteo.source = "openmeteo"
 
@@ -17,3 +41,24 @@ def get_multisource_meteo(city_name) -> MultiSourceModelRep:
             res_openweather
         ]
     )
+
+def get_multisource_meteo(city_name): 
+    """
+    Implémente la facade avec redis.
+    Quand on a une requete entrante, on regarde l'index redis
+    Si existe, on prend le res sinon on appelle la fonction et
+    on savaugarde sur redis.
+    """
+    s = datetime.datetime.now()
+    redis_key = make_redis_key(city_name)    
+    if redis_client.exists(redis_key):
+        # si existe, récupère le cache
+        res = redis_client.hgetall(redis_key).items().decode()
+    else:
+        # retourne un modele à dumper pour obtenir un dict
+        res = call_multisource_meteo(city_name).model_dump()
+        # on convertit le dict en bytes et on enregistre dans le cache
+        redis_client.hset(name="weather:current", key=redis_key, value=str(res).encode())
+    # on ajoute le temps de réponse de l'api
+    res["rep_duration"] = f"{(datetime.datetime.now() - s).total_seconds() * 1000:.2f} ms"
+    return res
