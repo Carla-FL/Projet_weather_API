6e0c93782619dc71d9d7b0da1e7e00b148c57781
Féréol Gbenou <fereolgbenou@MacBook-Air-de-Fereol.local>
2025-06-25 11:13:05 +0200

    added single route for querying mutlisourcemeteo

--- src/controllers/openmeteo.py
+++ src/controllers/openmeteo.py
@@ -1,38 +1,46 @@
-import httpx, dotenv, os
-from src. controllers.openweather import get_coord_from_city
 import openmeteo_requests
 from openmeteo_sdk.Variable import Variable
+from src. controllers.openweather import get_coord_from_city
 
-dotenv.load_dotenv()
-TOKEN = os.getenv("api_key_open_weather", "")
+# dotenv.load_dotenv()
+# TOKEN = os.getenv("api_key_open_weather", "")
 om = openmeteo_requests.Client()
 
-def get_weather_from_openmeteo(c):
-        fields = get_coord_from_city(c)
-        lon = fields.lon
-        lat = fields.lat
-        params = {
-                "latitude": lat,
-                "longitude": lon,
-                "temperature_unit": "celsius",
-                "current": ["apparent_temperature", "temperature_2m"],
-                "daily" :["temperature_2m_max","temperature_2m_min"]
-                }
-        res = om.weather_api("https://api.open-meteo.com/v1/forecast", params=params)
-        if res is None:
-                return None
-        else:
-                response = res[0]
-                current = response.Current()
-                current_variables = list(map(lambda i: current.Variables(i), range(0, current.VariablesLength())))
-                print(res)
-                print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-                print(f"Elevation {response.Elevation()} m asl")
-                print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-                print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
-                print(Variable.apparent_temperature)
-                current_apparent_temperature = next(filter(lambda x: x.Variable() == Variable.apparent_temperature, current_variables))
-                print(f"Variables : {current_variables} ")
-                current_temperature_2m = next(filter(lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2, current_variables))
-                print(f"Current apparent temperature: {current_apparent_temperature.Value()}°C")
-                return "ok"
+def get_weather_from_openmeteo(c) -> dict:
+    """
+    Faire un appel api vers la source openmeteo.
+    """
+
+    # etape 1 : récup les longitude et latitude en partant du nom de la ville
+    fields = get_coord_from_city(c)
+    lon = fields.lon
+    lat = fields.lat
+    ville = fields.name
+    pays = fields.state # ou fields.contry
+
+    # etape 2 : appel api en passant par le client
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "temperature_unit": "celsius",
+        "current": ["apparent_temperature", "temperature_2m"],
+        "daily" :["temperature_2m_max","temperature_2m_min"]
+        }
+    res = om.weather_api("https://api.open-meteo.com/v1/forecast", params=params)
+    if res is None:
+        return None
+    else:
+        response = res[0]
+        current = response.Current()
+        current_variables = list(map(lambda i: current.Variables(i), range(0, current.VariablesLength())))
+        print(res)
+        print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
+        print(f"Elevation {response.Elevation()} m asl")
+        print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
+        print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
+        print(Variable.apparent_temperature)
+        current_apparent_temperature = next(filter(lambda x: x.Variable() == Variable.apparent_temperature, current_variables))
+        print(f"Variables : {current_variables} ")
+        current_temperature_2m = next(filter(lambda x: x.Variable() == Variable.temperature and x.Altitude() == 2, current_variables))
+        print(f"Current apparent temperature: {current_apparent_temperature.Value()}°C")
+        return "ok"
--- src/services/api_fastapi/main.py
+++ src/services/api_fastapi/main.py
@@ -1,7 +1,9 @@
 import dotenv, os, fastapi, sys, uvicorn
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
-from src.controllers import openmeteo, openweather, weatherapi
 from fastapi.exceptions import HTTPException
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..')))
+from src.controllers import openmeteo, openweather, weatherapi, merge
+
 
 dotenv.load_dotenv()
 app = fastapi.FastAPI()
@@ -11,8 +13,8 @@ def start():
     return {"reponse": "welcome"}
 
 
-@app.get("/meteo/<city_name>")
-def load_meteo_from_city(city_name: str=None):
+@app.get("/meteo/openweather/<city_name>")
+def load_meteo_from_city_with_openweather(city_name: str=None):
     if not city_name:
         return {"error": "city name required !"} # ajouter status code erreur
     res = openweather.get_weather_from_city(city_name)
@@ -20,8 +22,9 @@ def load_meteo_from_city(city_name: str=None):
         raise HTTPException(status_code=404, detail="Weather data not found")
     return res
 
+
 @app.get("/meteo/openmeteo/<city_name>")
-def load_meteo_from_openmeteo(city_name: str=None):
+def load_meteo_from_city_with_openmeteo(city_name: str=None):
     if not city_name:
         return {"error": "city name required !"} # ajouter status code erreur
     res = openmeteo.get_weather_from_openmeteo(city_name)
@@ -29,6 +32,16 @@ def load_meteo_from_openmeteo(city_name: str=None):
     if res is None:
         raise HTTPException(status_code=404, detail="Weather data not found")
     return res
+
+
+@app.get("/meteo/multisource/<city_name>")
+def load_meteo_mutisource(city_name: str=""):
+    if not city_name:
+        raise HTTPException(status_code=400, detail="city name is required")
+    try:
+        return merge.get_multisource_meteo(city_name)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=f"Erreur dans get_multisource_meteo : {str(e)}")
 
 if __name__ == '__main__':
     uvicorn.run("src.services.api_fastapi.main:app", host="127.0.0.1", port=8025, reload=True)
